c7bd9f3b577dfa2e9b4969c4660f2c694e9796b0
henrietteharmse <henrietteharmse@gmail.com>
2023-10-19 09:53:47 +0100

    #1

--- generateOntology.sh
+++ generateOntology.sh
@@ -11,6 +11,16 @@ robot.sh extract --method TOP \
     --term-file dependentOntologies/dublin_core_terms.txt \
     --output results/dublin_core_module.owl
 
+robot.sh extract --method BOT \
+    --input dependentOntologies/duo.owl \
+    --term-file dependentOntologies/duo_terms.txt \
+    --output results/duo_module.owl
+
+robot.sh extract --method BOT \
+    --input dependentOntologies/EDAM.owl \
+    --term-file dependentOntologies/edam_terms.txt \
+    --output results/edam_module.owl
+
 robot.sh  extract --method TOP \
     --input dependentOntologies/foaf-20140114.rdf \
     --term-file dependentOntologies/foaf_terms.txt \
@@ -20,11 +30,6 @@ robot.sh extract --method BOT \
     --input dependentOntologies/sio-release.owl \
     --term-file dependentOntologies/sio_terms.txt \
     --output results/sio_module.owl
-
-robot.sh extract --method BOT \
-    --input dependentOntologies/duo.owl \
-    --term-file dependentOntologies/duo_terms.txt \
-    --output results/duo_module.owl
 
 robot.sh extract --method TOP \
     --input dependentOntologies/schemaorg.owl \
@@ -36,13 +41,15 @@ robot.sh extract --method STAR \
     --term-file dependentOntologies/vCard.txt \
     --output results/vcard_module.owl
 
-robot.sh template --template extension/ejprd-schema-template.csv \
+robot.sh -v template --template extension/ejprd-schema-template.csv \
   --ontology-iri "http://purl.org/ejp-rd/vocabulary/ejprd_resource_metadata_ontology.owl"  \
   --input dependentOntologies/dcat2.rdf \
   --prefix "ejprd: http://purl.org/ejp-rd/vocabulary/" \
   --prefix "dcat: http://www.w3.org/ns/dcat#" \
   --prefix "duo: http://purl.obolibrary.org/obo/" \
+  --prefix "edam: http://purl.obolibrary.org/obo/" \
   --prefix "owl:http://www.w3.org/2002/07/owl#" \
+  --prefix "rdfs:http://www.w3.org/2000/01/rdf-schema#" \
   --prefix "xsd:http://www.w3.org/2001/XMLSchema#" \
   --prefix "schema:https://schema.org/" \
   --prefix "vcard:http://www.w3.org/2006/vcard/ns#" \
@@ -52,9 +59,10 @@ robot.sh merge --include-annotations true \
   --input extension/extension.owl \
   --input results/dcat2_module.owl \
   --input results/dublin_core_module.owl \
+  --input results/duo_module.owl \
+  --input results/edam_module.owl \
   --input results/foaf_module.owl \
   --input results/sio_module.owl \
-  --input results/duo_module.owl \
   --input results/schemaorg_module.owl \
   --input results/vcard_module.owl \
   --output ejprd_resource_metadata_ontology.owl
